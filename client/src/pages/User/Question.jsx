121971122fdabeb84563d9197fe2a8123dff2ab2
Sukhi898 <146666624+Sukhi898@users.noreply.github.com>
2024-09-03 18:41:05 +0530

    Update Question.jsx

--- client/src/pages/User/Question.jsx
+++ client/src/pages/User/Question.jsx
@@ -3,20 +3,22 @@ import Option from "./Option";
 import styles from "./styles/Question.module.css";
 
 export default function Question({ question, addResult }) {
-  console.log(question);
   const [selected, setSelected] = useState(null);
 
   const handleSelect = (index) => {
     const result = { questionId: question._id, selectedOption: index };
-
     addResult(result);
     setSelected(index);
   };
+  console.log(question.optionsType);
+  const optionsClass =
+    question.optionsType === "text" ? styles.option2 : styles.option;
+  console.log(optionsClass);
 
   return (
     <div className="">
       <p className={styles.question}>{question?.question}</p>
-      <div className={styles.options}>
+      <div className={`${styles.options} ${optionsClass}`}>
         {question.options?.map((op, index) => (
           <Option
             selected={selected}
