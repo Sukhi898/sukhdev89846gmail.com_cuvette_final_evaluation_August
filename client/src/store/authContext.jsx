f01752ef1e802e250c1d3a7ab2d675c0156e5fc3
Sukhi898 <146666624+Sukhi898@users.noreply.github.com>
2024-09-20 01:49:03 +0530

    Update authContext.jsx

--- client/src/store/authContext.jsx
+++ client/src/store/authContext.jsx
@@ -7,7 +7,7 @@ import {
 } from "react";
 
 export const AuthContext = createContext({
-  user: "",
+  user: null,
   saveUser: () => {},
   logout: () => {},
   isLoading: true,
@@ -17,17 +17,34 @@ export default function AuthProvider({ children }) {
   const [user, setUser] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
 
+  const isTokenExpired = (token) => {
+    try {
+      const base64Url = token.split(".")[1];
+      const base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+      const decodedPayload = JSON.parse(window.atob(base64));
+
+      return decodedPayload.exp * 1000 < Date.now();
+    } catch (error) {
+      return true; 
+    }
+  };
+
   useEffect(() => {
     const savedUser = localStorage.getItem("userToken");
     if (savedUser) {
-      setUser(savedUser);
+      if (isTokenExpired(savedUser)) {
+        localStorage.removeItem("userToken");
+        setUser(null);
+      } else {
+        setUser(savedUser);
+      }
     }
     setIsLoading(false);
   }, []);
 
-  const saveUser = useCallback((data) => {
-    setUser(data);
-    localStorage.setItem("userToken", data);
+  const saveUser = useCallback((token) => {
+    setUser(token);
+    localStorage.setItem("userToken", token);
   }, []);
 
   const logout = useCallback(() => {
